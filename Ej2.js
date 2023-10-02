489f41cd563a96c99a54e9ae4e24d2757e737d00
pedrodmm63 <pedrodmm63@educastur.es>
2023-10-02 13:51:03 +0200

    Ej2

---
+++ Ej2.js
@@ -0,0 +1,12 @@
+var x = prompt("Escriba el primer numero");
+var y = prompt("Escriba el segundo numero");
+
+console.log("El numero redondeado al alza de " + y + " es " + Math.ceil(y));
+console.log("El numero redondeado a la baja de " + x + " es " + Math.floor(x));
+console.log("El entero mas proximo de " + y + " es " + Math.round(y));
+console.log("Un valor aletorio es " + Math.random());
+console.log("El valor de " + x + " elevado a " + y + " es " + Math.pow(x, y));
+console.log("La raiz cuadrada de " + y + " es " + Math.sqrt(y));
+console.log("El maximo entre " + x + " y " + y + " es " + Math.max(x, y));
+console.log("El minimo entre " + x + " y " + y + " es " + Math.min(x, y));
+
